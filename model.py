6a11d95a3c1cd80b9dcc812456e256585fa792e5
sariq16 <sariq16@gmail.com>
2024-03-31 06:18:29 -0400

    added code for ml models

--- model.py
+++ model.py
@@ -1,2 +1,13 @@
 print('adding all ml model code here')
 
+def model1():
+
+    print("added code for model1")
+
+def model2():
+
+    print("added code for model2")
+
+def model3():
+
+    print("added code for model3")
